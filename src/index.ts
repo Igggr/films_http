8bf29d44addcf35390b8b0b3ee349bd5f47a1c66
Igor Smirnov <uchetzapis@yahoo.com>
2023-03-09 23:22:30 +0300

    Use eslint

--- src/index.ts
+++ src/index.ts
@@ -7,6 +7,6 @@ config();
 const PORT = process.env.APP_PORT;
 const server = createServer();
 
-server.on('request', (req, res) => { res.end("hello") });
+server.on('request', (req, res) => { res.end("hello"); });
 
 server.listen(PORT, () => console.log(`running on port ${PORT}`));
